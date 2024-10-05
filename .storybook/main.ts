478e816253584c45444cb9ce74777d3c512c9461
Adrian Mayor <104377546+AdrianMayor@users.noreply.github.com>
2024-10-05 20:13:10 +0200

    Styles created - storybook init

---
+++ .storybook/main.ts
@@ -0,0 +1,36 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["..\\public"],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+
+    // Allows importing sass or scss files
+    config.module?.rules?.push({
+      test: /\.scss|.sass$/,
+      use: ["style-loader", "css-loader", "sass-loader", "scss-loader"],
+      include: path.resolve(__dirname, "@/*"),
+    });
+
+    // Return the altered config
+    return config;
+  },
+};
+export default config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    sassOptions: {
+        prependData: `
+            @use './src/app/styles/base/_typography.scss';
+            @use './src/app/styles/base/_palette.scss';
+          `,
+      },
+};
 
 export default nextConfig;
