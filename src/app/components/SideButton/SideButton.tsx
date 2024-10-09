bdcdfd51d08a09b28db3e31f91ec3cf03ad33fc3
Adrian Mayor <104377546+AdrianMayor@users.noreply.github.com>
2024-10-09 18:09:40 +0200

    Adding dynamic icons to sideButton menu

--- src/app/components/SideButton/SideButton.tsx
+++ src/app/components/SideButton/SideButton.tsx
@@ -1,21 +1,42 @@
 import styles from "./SideButton.module.scss";
 
 const { sideButton, sideButton_active } = styles;
-console.log(styles);
-export interface SideButtonProps {
+
+export enum ButtonType {
+	defaultButton = "defaultButton",
+	homeButton = "homeButton",
+	potsButton = "potsButton",
+	budgetsButton = "budgetsButton",
+	recurringButton = "recurringButton",
+	transactionsButton = "transactionsButton",
+}
+
+interface SideButtonProps {
+	/**Disable button if loading */
+	loading: boolean;
 	/** Active state */
 	active?: boolean;
 	/** Text that is going to contain the button */
 	label?: string;
 	/**  Optional click handler */
 	onClick?: () => void;
+	/** Button type */
+	type?: ButtonType;
 }
 
-const SideButton = ({ active = false, label = "Placeholder", onClick }: SideButtonProps) => {
+const SideButton = ({
+	loading = false,
+	active = false,
+	label = "Placeholder",
+	onClick,
+	type = ButtonType.defaultButton,
+}: SideButtonProps) => {
 	const currentStyle = active ? `${sideButton} ${sideButton_active}` : sideButton;
 
+	const typeClass = styles[type];
+
 	return (
-		<button className={currentStyle} onClick={onClick}>
+		<button className={`${currentStyle} ${typeClass}`} disabled={loading} onClick={onClick}>
 			{label}
 		</button>
 	);
