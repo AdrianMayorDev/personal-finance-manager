0e029b6bed8ffa843d0eb3fd9e4c8d204cf7698d
Adrian Mayor <104377546+AdrianMayor@users.noreply.github.com>
2024-10-13 16:16:41 +0200

    introducing select type to InputField component

--- src/app/components/InputField/InputField.tsx
+++ src/app/components/InputField/InputField.tsx
@@ -12,15 +12,31 @@ interface IInputFieldProps {
 	handleOnChange?: () => void;
 	helper?: boolean;
 	helperText?: string;
+	icon?: "icon" | "circleIcon";
 }
 
-const InputField = ({ type, name, label, value, placeholder, handleOnChange, helper, helperText }: IInputFieldProps) => {
+const InputField = ({ type, name, label, value, placeholder, handleOnChange, helper, helperText, icon, options }: IInputFieldProps) => {
+	const iconTypeClass = icon ? styles[icon] : "";
+
+	const inputType =
+		type === "text" ? (
+			<input id={name} name={name} type={type} value={value} placeholder={placeholder} onChange={handleOnChange} />
+		) : (
+			<select id={name} name={name} value={value} onChange={handleOnChange}>
+				{options?.map((option) => (
+					<option key={option.value} value={option.value}>
+						{option.label}
+					</option>
+				))}
+			</select>
+		);
+
 	return (
 		<div className={inputFieldContainer}>
 			<label htmlFor={name}>{label}</label>
 			<div>
-				<div className={styles.icon}></div>
-				<input id={name} name={name} type={type} value={value} placeholder={placeholder} onChange={handleOnChange} />
+				<div className={iconTypeClass}></div>
+				{inputType}
 			</div>
 			{helper && <span>{helperText}</span>}
 		</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,7 @@ import "./styles/main.scss";
 import InputField from "./components/InputField/InputField";
 
 export default function Home() {
-	return <InputField label='Label test' name='name' type='text' placeholder='test placeholder' helper helperText='Helper text' />;
+	return (
+		<InputField label='Label test' name='name' type='text' placeholder='test placeholder' helper helperText='Helper text' icon='icon' />
+	);
 }
